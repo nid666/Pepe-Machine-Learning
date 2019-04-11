3ea875089a62aab18a1deb42b49bdc28df0a6bcf
nid666 <35160315+nid666@users.noreply.github.com>
2019-04-11 11:12:58 -0400

    Update scraper.py
    Fixed the code for the image scraper - still cant set max images and path

--- scraper.py
+++ scraper.py
@@ -1,7 +1,7 @@
 #pip install ImageScraper
 import image_scraper
 #this uses python3
-max = input("How many pepe images do you want to download")
+max = input("How many pepe images do you want to download - not currently working")
 source = input("From which sources should images be imported")
-path = input("Add the path of test_images folder")
-image-scraper -s -m max source
+path = input("Add the path of test_images folder - not currently working")
+image_scraper.scrape_images(source)
